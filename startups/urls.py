d0541c31a8e4d672966a997ed389e2ed1db99599
MehdNizar <mehdaoui.nizar@etu.uae.ac.ma>
2026-01-15 22:10:15 +0100

    Delete startups directory

--- startups/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('startup/<int:pk>/', views.startup_detail, name='startup_detail'),
-    path('add/', views.add_startup, name='add_startup'),
-]
